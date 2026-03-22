07ea709661737648c8991ebfff77fa72c0f9ac8a
Noav <noav@itu.dk>
2026-03-22 15:00:42 +0100

    Finished Assignment 6. Added assignment 7 boilerplate

---
+++ Assignment7/Assignment7/Eval.fs
@@ -0,0 +1,9 @@
+module Interpreter.Eval
+
+    open Result
+    open Language
+    open State
+    
+    let aexprEval _ = failwith "not implemented"    
+    let bexprEval _ = failwith "not implemented"
+    let stmntEval _ = failwith "not implemented"
