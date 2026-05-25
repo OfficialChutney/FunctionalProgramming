0ea855c78b8016f51e1d96eba25ab7780bc6640d
Noav <noav@itu.dk>
2026-05-25 20:56:01 +0200

    MADE A SHIT TON OF EXERCISES

--- Assignment10/Eval.fs
+++ Assignment10/Eval.fs
@@ -7,7 +7,7 @@ module Interpreter.Eval
     type StateBuilder() =  
 
         member this.Bind(f, x) = (>>=) f x  
-        member this.Return(x) = ret x 
+        member this.Return(x) = ret x  
         member this.ReturnFrom(x) = x  
         member this.Combine(a, b) = a >>= (fun _ -> b) 
       
@@ -38,28 +38,28 @@ module Interpreter.Eval
         | Not n -> "(not " + bexprToString n + ")"
 
 
-    let rec aexprEval2 (expr : aexpr) = ev {
+    let rec arithEval2 (expr : aexpr) = ev {
         match expr with
         | MemRead a ->
-            let! x = aexprEval2 a
+            let! x = arithEval2 a
             return! getMem x
 
         | Var x -> return! getVar x
         | Num n -> return n
         | Add (a,b) ->
-            let! x = aexprEval2 a
-            let! y = aexprEval2 b
+            let! x = arithEval2 a
+            let! y = arithEval2 b
 
             return (x + y)
             
         | Mul (a,b) ->
-            let! x = aexprEval2 a
-            let! y = aexprEval2 b
+            let! x = arithEval2 a
+            let! y = arithEval2 b
 
             return (x * y)
         | Div (a,b) ->
-            let! x = aexprEval2 a
-            let! y = aexprEval2 b
+            let! x = arithEval2 a
+            let! y = arithEval2 b
 
 
             if y <> 0 then return (x / y) else return! fail DivisionByZero
@@ -67,68 +67,68 @@ module Interpreter.Eval
         | Random -> return! random
         | Read -> return (readInt ())
         | Cond (b, a1, a2) ->
-            let! x = bexprEval2 b
+            let! x = boolEval2 b
             match x with
-            | true -> return! aexprEval2 a1
-            | false -> return! aexprEval2 a2
+            | true -> return! arithEval2 a1
+            | false -> return! arithEval2 a2
         | _ -> return 0
     }
-    and bexprEval2 (bexpr : bexpr) = ev {
+    and boolEval2 (bexpr : bexpr) = ev {
         match bexpr with
         | TT -> return true
         | Not a -> 
-            let! x = bexprEval2 a
+            let! x = boolEval2 a
             return (not x)
         | Eq (a,b) -> 
-            let! x = aexprEval2 a
-            let! y = aexprEval2 b
+            let! x = arithEval2 a
+            let! y = arithEval2 b
 
             return (x = y)        
         | Lt (a,b) -> 
-            let! x = aexprEval2 a
-            let! y = aexprEval2 b
+            let! x = arithEval2 a
+            let! y = arithEval2 b
             return (x < y)
         | Conj (a,b) -> 
-            let! x = bexprEval2 a
-            let! y = bexprEval2 b
+            let! x = boolEval2 a
+            let! y = boolEval2 b
 
             return (x && y)        
     }
     
 
-    let rec aexprEval (expr : aexpr) =
+    let rec arithEval (expr : aexpr) =
         match expr with
         | MemRead a ->
-            aexprEval a >>= fun x -> getMem x
+            arithEval a >>= fun x -> getMem x
         | Var x -> getVar x
         | Num n -> ret n
         | Add (a,b) ->
-            aexprEval a >>= fun x ->
-            aexprEval b >>= fun y ->
+            arithEval a >>= fun x ->
+            arithEval b >>= fun y ->
             ret (x + y)
         | Mul (a,b) ->
-            aexprEval a >>= fun x ->
-            aexprEval b >>= fun y ->
+            arithEval a >>= fun x ->
+            arithEval b >>= fun y ->
             ret (x * y)
         | Div (a,b) ->
-            aexprEval a >>= fun x ->
-            aexprEval b >>= fun y ->
+            arithEval a >>= fun x ->
+            arithEval b >>= fun y ->
             if y <> 0 then ret (x / y) else fail DivisionByZero
         | Random -> random
         | Read -> ret (readInt ())
         | Cond (b, a1, a2) ->
-            bexprEval b >>= fun x ->
+            boolEval b >>= fun x ->
             match x with
-            | true -> aexprEval a1
-            | false -> aexprEval a2
+            | true -> arithEval a1
+            | false -> arithEval a2
         | _ -> ret 0
-    and bexprEval (bexpr : bexpr) =
+    and boolEval (bexpr : bexpr) =
         match bexpr with
         | TT -> ret true
-        | Not a -> bexprEval a >>= fun x -> ret (not x)
-        | Eq (a,b) -> aexprEval a >>= fun x -> aexprEval b >>= fun y -> ret (x = y)
-        | Lt (a,b) -> aexprEval a >>= fun x -> aexprEval b >>= fun y -> ret (x < y)
-        | Conj (a,b) -> bexprEval a >>= fun x -> bexprEval b >>= fun y -> ret (x && y)
+        | Not a -> boolEval a >>= fun x -> ret (not x)
+        | Eq (a,b) -> arithEval a >>= fun x -> arithEval b >>= fun y -> ret (x = y)
+        | Lt (a,b) -> arithEval a >>= fun x -> arithEval b >>= fun y -> ret (x < y)
+        | Conj (a,b) -> boolEval a >>= fun x -> boolEval b >>= fun y -> ret (x && y)
 
 
     let mergeStrings (es : aexpr list) (s : string) =
@@ -140,7 +140,7 @@ module Interpreter.Eval
             match es with
             | [] -> ret acc
             | x::xs ->
-                aexprEval x >>= fun y -> evaluateValues xs (y::acc)
+                arithEval x >>= fun y -> evaluateValues xs (y::acc)
 
         
         let rec mergeStringsA (es : int list) (s : string list) (acc : string) =
@@ -240,24 +240,24 @@ module Interpreter.Eval
     let rec stmntEval (stmnt : stmnt) = 
         match stmnt with
         | Alloc (x, e) ->
-            aexprEval e >>= fun size -> alloc x size
+            arithEval e >>= fun size -> alloc x size
         | Free (e1, e2) ->
-            aexprEval e1 >>= fun ptr -> aexprEval e2 >>= fun size -> free ptr size
+            arithEval e1 >>= fun ptr -> arithEval e2 >>= fun size -> free ptr size
         | MemWrite (e1, e2) ->
-            aexprEval e1 >>= fun ptr -> aexprEval e2 >>= fun v -> setMem ptr v
+            arithEval e1 >>= fun ptr -> arithEval e2 >>= fun v -> setMem ptr v
         | Skip -> ret ()
         | Declare v -> declare v
         | Assign (v,a) -> 
-            aexprEval a >>= fun a -> setVar v a
+            arithEval a >>= fun a -> setVar v a
         | Seq (s1, s2) -> 
             stmntEval s1 >>= fun st1 -> stmntEval s2
         | If (bexpr,s1,s2) -> 
-            bexprEval bexpr >>= fun x ->
+            boolEval bexpr >>= fun x ->
             match x with
             | true -> stmntEval s1
             | false -> stmntEval s2
         | While (bexpr, s) ->
-            bexprEval bexpr >>= fun x ->
+            boolEval bexpr >>= fun x ->
             match x with
             | true -> stmntEval s >>= fun y -> stmntEval (While (bexpr, s))
             | false -> ret ()
@@ -272,26 +272,26 @@ module Interpreter.Eval
     let rec stmntEval2 (stmnt : stmnt) = ev {
         match stmnt with
         | Alloc (x, e) ->
-            let! size = aexprEval e
+            let! size = arithEval e
 
             return! alloc x size
         | Free (e1, e2) ->
-            let! ptr = aexprEval e1
-            let! size = aexprEval e2
+            let! ptr = arithEval e1
+            let! size = arithEval e2
 
             return! free ptr size
 
         | MemWrite (e1, e2) ->
 
-            let! ptr = aexprEval e1
+            let! ptr = arithEval e1
 
-            let! v = aexprEval e2
+            let! v = arithEval e2
 
             return! setMem ptr v
         | Skip -> return ()
         | Declare v -> return! declare v
         | Assign (v,a) -> 
-            let! a = aexprEval a
+            let! a = arithEval a
             return! setVar v a
         | Seq (s1, s2) -> 
 
@@ -299,12 +299,12 @@ module Interpreter.Eval
 
             return! stmntEval s2
         | If (bexpr,s1,s2) -> 
-            let! x = bexprEval bexpr
+            let! x = boolEval bexpr
             match x with
             | true -> return! stmntEval s1
             | false -> return! stmntEval s2
         | While (bexpr, s) ->
-            let! x = bexprEval bexpr
+            let! x = boolEval bexpr
             match x with
             | true -> 
                 let! y = stmntEval s
---
+++ Exams/Reexam2023/FPReExam2023/Exam.fs
@@ -0,0 +1,368 @@
+﻿module ReExam2023
+
+(* If you are importing this into F# interactive then comment out
+   the line above and remove the comment for the line bellow.
+
+   Do note that the project will not compile if you do this, but 
+   it does allow you to work in interactive mode and you can just remove the '=' 
+   to make the project compile again.
+
+   You will also need to load JParsec.fs. Do this by typing
+   #load "JParsec.fs" 
+   in the interactive environment. You may need the entire path.
+
+   Do not remove the module declaration (even though that does work) because you may inadvertently
+   introduce indentation errors in your code that may be hard to find if you want
+   to switch back to project mode. 
+
+   Alternative, keep the module declaration as is, but load ExamInteractive.fsx into the interactive environment
+   *)
+(*
+ module ReExam2023 = 
+ *)
+
+(* 1: Arithmetic *)
+    
+    type arith =
+    | Num of int
+    | Add of arith * arith
+    
+    let p1 = Num 42
+    let p2 = Add(Num 5, Num 3)
+    let p3 = Add(Add(Num 5, Num 3), Add(Num 7, Num (-9)))
+    
+(* Question 1.1: Evaluation *)
+    let rec eval (a : arith) = 
+        match a with
+        | Num i-> i
+        | Add (num1, num2) -> eval num1 + eval num2
+    
+(* Question 1.2: Negation and subtraction *)
+    let rec negate (a : arith) = 
+        match a with
+        | Num a -> Num -a
+        | Add (a1, a2) -> Add (negate a1, negate a2)
+    let rec subtract (a : arith) (b : arith) = 
+        match a,b with
+        | Num a, Num b -> Add (Num a, Num -b)
+        | Add (a1, a2), Add (b1,b2) -> Add (Add (a1, a2), subtract b1 b2)
+        | Add (a1, a2), Num b -> Add (Add (a1, a2), Num -b)
+        | Num a, Add(b1, b2) -> Add(Num a, subtract b1 b2)
+
+(* Question 1.3: Multiplication *)
+        
+    let rec multiply (a : arith) (b : arith) = 
+        match a,b with
+        | Num a, Num b -> Num (a*b)
+        | Num a, Add(b1, b2) -> Add (multiply (Num a) b1, multiply (Num a) b2)
+        | Add (a1, a2), b -> Add (multiply a1 b, multiply a2 b)
+    
+(* Question 1.4: Exponents *)
+
+    let pow (a : arith) (b : arith) = 
+
+        let rec aux (a : arith) (b : arith) (acc : arith) =
+            match eval b with
+            | 1 -> multiply a acc
+            | _ -> aux a (subtract b (Num 1)) (multiply a acc)
+
+        aux a b (Num 1)
+
+    
+
+
+(* Question 1.5: Iteration *)
+
+    let rec iterate (f : ('a -> 'a)) (acc : 'a) (a : arith) = 
+        match eval a with
+        | 0 -> acc
+        | _ -> iterate f (f acc) (subtract a (Num 1))
+        
+    let pow2 (a : arith) (b : arith) = iterate (fun x -> multiply a x) (Num 1) b
+    
+(* 2: Code Comprehension *)
+ 
+    let rec foo =
+        function
+        | 0            -> true
+        | x when x > 0 -> bar (x - 1)
+        | x            -> bar (x + 1)
+        
+    and bar =
+        function
+        | 0            -> false
+        | x when x > 0 -> foo (x - 1)
+        | x            -> foo (x + 1)
+        
+    let rec baz =
+        function
+        | []                 -> [], []
+        | x :: xs when foo x ->
+            let ys, zs = baz xs
+            (x::ys, zs)
+        | x :: xs ->
+            let ys, zs = baz xs
+            (ys, x::zs)
+        
+
+(* Question 2.1: Types, names and behaviour *)
+
+    (* 
+    
+    Q: What are the types of functions foo, bar, and baz?
+
+    A: foo and bar are mutually recursive. Foo and bar are tail recursive, using an int as an accumulator.
+
+    Baz is type inferred and is also non tail recursive.
+    Baz is not mutually recursive.
+
+
+    Q: What do the function foo, bar, and baz do.
+       Focus on what they do rather than how they do it.
+
+    A: 
+    foo returns true if it is an even number.
+    bar returns true if it is an odd number.
+
+    Baz takes in a list of integers and returns a tuple with two list. The first element is a list of even numbers.
+    The second is a list of odd numbers.
+    
+    Q: What would be appropriate names for functions 
+       foo, bar, and baz?
+    
+    A: They could be called, taken into account the name is what is called FIRST:
+    foo = isEven
+    bar = isOdd
+    baz = SplitEvenOdd
+        
+    *)
+        
+
+(* Question 2.2: Code snippets *)
+
+ 
+    (* 
+    The function baz contains the following three code snippets. 
+
+    * A: `baz xs`
+    * B: `bar x`
+    * C: `(ys, x::zs)`
+
+    Q: In the context of the baz function, i.e. assuming that `x`, `xs`, `ys`, and `zs` all have the correct types,
+       what are the types of snippets A, B, and C, expressed using the F# syntax for types, and what are they -- 
+       focus on what they do rather than how they do it.
+    
+    A: 
+        A is: list<int> * list<int> - Baz returns a tuple of list of integers.
+            When calling baz, it recurses down until an empty list, 
+            and splits each number of the input list into a tuple of either odd or even numbers.
+        B is: bool - Bar returns a bool. Bar returns true if the input number is odd, and false if it is even.
+        C is: has type list<int> * list<int>. 
+            Here we create a tuple with ys in the fst, and in the snd we add the integer x to the head of list zs.
+            We end up with a tuple of two lists of integers.
+
+    
+    Q: * Explain the use of the `and`-operator that connect the `foo` and the `bar` functions.
+       * Argue if the program would work if you replaced `and` with `let rec`.
+
+    A: and makes the two mutually recursive.
+        This means they can both be used, and are compiled on the same "level"
+        so they both can call eachother. Replacing 'and' with 'let rec' would not work,
+        as foo would not be able to call bar, as bar is defined later down, so bar would be undefined.
+
+    *)
+
+(* Question 2.3: No recursion *) 
+
+    let foo2 (a : int) = a % 2 = 0
+    let bar2 (a : int) = a % 2 = 1
+
+(* Question 2.4: Tail Recursion *)
+
+    (*
+
+    Q: The function `baz` is not tail recursive. Demonstrate why.
+       To make a compelling argument you should evaluate a function call of the function,
+       similarly to what is done in Chapter 1.4 of HR, and reason about that evaluation. 
+       You need to make clear what aspects of the evaluation tell you that the function 
+       is not tail recursive. Keep in mind that all steps in an evaluation chain must 
+       evaluate to the same value ( (5 + 4) * 3 --> 9 * 3 --> 27 , for instance).
+       
+       You do not have to step through the foo- or the bar-functions. You are allowed to evaluate 
+       those function immediately.
+        let rec baz =
+        function
+        | []                 -> [], []
+        | x :: xs when foo x ->
+            let ys, zs = baz xs
+            (x::ys, zs)
+        | x :: xs ->
+            let ys, zs = baz xs
+            (ys, x::zs)
+
+    A: 
+    
+    *)
+(* Question 2.5: Continuations *)
+
+    let bazTail _ = failwith "not implemented"
+
+(* 3: Balanced brackets *)
+
+      
+    let explode (str : string) = [for c in str -> c]
+    let implode (lst : char list) = lst |> List.toArray |> System.String
+    
+(* Question 3.1: Balanced brackets *)
+    
+    let balanced _ = failwith "not implemented"
+        
+(* Question 3.2: Arbitrary delimiters *)
+    
+    let balanced2 _ = failwith "not implemented"
+    
+(* Question 3.3: Matching brackets and palindromes *)    
+    
+    let balanced3 _ = failwith "not implemented" 
+    
+    let symmetric _ = failwith "not implemented"
+        
+(* Question 3.4: Parsing balanced brackets *)    
+               
+    open JParsec.TextParser
+    
+        
+    let ParseBalanced, bref = createParserForwardedToRef<unit>()
+    
+    let parseBalancedAux = pstring "Your parser goes here"
+        
+    // uncomment after you have done parseBalancedAUX
+    
+    // let parseBalanced = parseBalancedAux .>> pstring "**END**"
+    // do bref := parseBalancedAux
+            
+(* Question 3.5: Parallel counting *)
+
+    let countBalanced _ = failwith "not implemented"
+
+(* 4: BASIC *)
+    
+    
+    type var = string
+
+    type expr =  
+    | Num    of int              // Integer literal
+    | Lookup of var              // Variable lookup
+    | Plus   of expr * expr      // Addition
+    | Minus  of expr * expr      // Subtraction
+    
+    type stmnt =
+    | If of expr * uint32       // Conditional statement (if-then (no else)).
+    | Let of var * expr        // Variable update/declaration
+    | Goto of uint32           // Goto
+    | End                      // Terminate program
+      
+    type prog = (uint32 * stmnt) list  // Programs are sequences of commands with their own line numbers 
+
+    
+    let (.+.) e1 e2 = Plus(e1, e2)  
+    let (.-.) e1 e2 = Minus(e1, e2)  
+    
+    let fibProg xarg =  
+        [(10u, Let("x",    Num xarg))                         // x = xarg
+         (20u, Let("acc1", Num 1))                            // acc1 = 1
+         (30u, Let("acc2", Num 0))                            // acc2 = 0
+         
+         (40u, If(Lookup "x", 60u))                           // if x > 0 then goto 60 (start loop)
+         
+         (50u, Goto 110u)                                     // Goto 110 (x = 0, terminate program)
+         
+         (60u, Let ("x", Lookup "x" .-. Num 1))               // x = x - 1
+         (70u, Let ("result", Lookup "acc1"))                 // result = acc1
+         (80u, Let ("acc1", Lookup "acc1" .+. Lookup "acc2")) // acc1 = acc1 + acc2
+         (90u, Let ("acc2", Lookup "result"))                 // acc2 = result
+         (100u, Goto 40u)                                     // Goto 40u (go to top of loop)
+         
+         (110u, End)                                          // Terminate program
+                                                              // the variable result contains the
+                                                              // fibonacci number of xarg
+         ]
+
+(* Question 4.1: Basic programs *)
+
+    type basicProgram = Map<uint32, stmnt>
+    
+    let mkBasicProgram _ = failwith "not implemented"
+    let getStmnt _ = failwith "not implemented"
+    
+    let nextLine _ = failwith "not implemented"
+    
+    let firstLine _ = failwith "not implemented"
+    
+(* Question 4.2: State *)
+
+    type state = unit // Replace by your type type goes here
+    
+    let emptyState _ = failwith "not implemented"
+    
+    
+    let goto _ = failwith "not implemented"
+
+    let getCurrentStmnt _ = failwith "not implemented"
+    
+    let update _ = failwith "not implemented"
+    
+    let lookup _ = failwith "not implemented"
+    
+    
+(* Question 4.3: Evaluation *)
+    
+    let evalExpr _ = failwith "not implemented"
+    
+    
+    let step _ = failwith "not implemented"
+  
+        
+    let evalProg _ = failwith "not implemented"
+    
+(* Question 4.4: State monad *)
+    type StateMonad<'a> = SM of (basicProgram -> state -> 'a * state)  
+      
+    let ret x = SM (fun _ s -> (x, s))
+    
+    let bind f (SM a) : StateMonad<'b> =   
+        SM (fun p s ->
+            let x, s' = a p s
+            let (SM g) = f x
+            g p s')
+          
+    let (>>=) x f = bind f x  
+    let (>>>=) x y = x >>= (fun _ -> y)  
+      
+    let evalSM p (SM f) = f p (emptyState p)
+
+    let goto2 _ = failwith "not implemented"
+    
+    let getCurrentStmnt2 _ = failwith "not implemented"
+    
+    
+    let lookup2 _ = failwith "not implemented"
+    let update2 _ = failwith "not implemented"
+    
+    let step2 _ = failwith "not implemented"
+
+(* Question 4.5: State monad evaluation *)
+
+    type StateBuilder() =
+
+        member this.Bind(f, x)    = bind x f
+        member this.Return(x)     = ret x
+        member this.ReturnFrom(x) = x
+        member this.Combine(a, b) = a >>= (fun _ -> b)
+
+    let state = StateBuilder()
+
+    let evalExpr2 _ = failwith "not implemented"
+    
+    let evalProg2 _ = failwith "not implemented"
+        
